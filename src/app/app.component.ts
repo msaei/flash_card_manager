dbc45667553e84d5a251709fd693d0ae4bf1c77e
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2017-08-14 15:39:58 -0400

    feat(fireAuth): fire auth added to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public user: Observable<firebase.User>;
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  public login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  public logout() {
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+apiKey: "AIzaSyCtcxcrTXFQ28PYj25XURn_qVz26FyAPVU",
+    authDomain: "flashcard-manager.firebaseapp.com",
+    databaseURL: "https://flashcard-manager.firebaseio.com",
+    projectId: "flashcard-manager",
+    storageBucket: "",
+    messagingSenderId: "702143581804"
+    };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
